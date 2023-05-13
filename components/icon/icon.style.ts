43966ca64efce16e825ae24dbd0f8f9922ed12d1
Yunus <yunus@cntech.io>
2023-05-13 16:21:38 +0300

    icon fix

--- components/icon/icon.style.ts
+++ components/icon/icon.style.ts
@@ -1,5 +1,5 @@
-import {StyleSheet, ViewStyle} from 'react-native';
-import {Size, Theme} from '../style';
+import { StyleSheet, ViewStyle } from "react-native";
+import { Size, Theme } from "../style";
 
 type Props = {
   size: Size;
@@ -11,13 +11,14 @@ const iconStyle = (props: Props) =>
     root: {
       width: Theme.iconSize[props.size],
       height: Theme.iconSize[props.size],
+      justifyContent: "center",
+      alignItems: "center",
       ...props.style,
-      backgroundColor: 'red',
     },
     image: {
-      resizeMode: 'contain',
-      width: Theme.iconSize[props.size] - 2 * Theme.padding.none,
-      height: Theme.iconSize[props.size] - 2 * Theme.padding.none,
+      resizeMode: "contain",
+      width: Theme.iconSize[props.size] - 4,
+      height: Theme.iconSize[props.size] - 4,
     },
   });
 
