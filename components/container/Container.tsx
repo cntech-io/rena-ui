968ead1f01a97b70dbd6cb16a9301d6caffbf96c
Yunus <yunus@cntech.io>
2023-05-19 16:40:47 +0300

    linear gradient added for container

--- components/container/Container.tsx
+++ components/container/Container.tsx
@@ -1,7 +1,8 @@
-import React, {ReactNode} from 'react';
-import {SafeAreaView, View, ViewStyle} from 'react-native';
-import {Size} from '../style';
-import containerStyle from './container.style';
+import React, { ReactNode } from "react";
+import { SafeAreaView, View, ViewStyle } from "react-native";
+import { Size } from "../style";
+import containerStyle from "./container.style";
+import LinearGradient from "react-native-linear-gradient";
 
 type ContainerProps = {
   safeAreaFlag?: boolean;
@@ -10,6 +11,7 @@ type ContainerProps = {
   headerSize: Size;
   style?: ViewStyle;
   headerComponent?: ReactNode;
+  gradientColors?: string[];
 };
 
 type ContainerState = {};
@@ -21,14 +23,26 @@ export default class Container extends React.Component<
   style = containerStyle({
     paddingSize: this.props.paddingSize,
     headerSize: this.props.headerSize,
+    style: this.props.style,
   });
 
   render(): JSX.Element {
-    const {children, safeAreaFlag, headerComponent, ...styleProps} = this.props;
+    const { children, safeAreaFlag, headerComponent, ...styleProps } =
+      this.props;
 
     const _style = containerStyle(styleProps);
 
     if (safeAreaFlag) {
+      if (this.props.gradientColors && this.props.gradientColors.length > 1) {
+        <LinearGradient colors={this.props.gradientColors} style={_style.root}>
+          <SafeAreaView style={[_style.safeArea]}>
+            {headerComponent && (
+              <View style={_style.headerContainer}>{headerComponent}</View>
+            )}
+            <View style={_style.container}>{children}</View>
+          </SafeAreaView>
+        </LinearGradient>;
+      }
       return (
         <View style={_style.root}>
           <SafeAreaView style={[_style.safeArea]}>
@@ -39,6 +53,11 @@ export default class Container extends React.Component<
           </SafeAreaView>
         </View>
       );
+    }
+    if (this.props.gradientColors && this.props.gradientColors.length > 1) {
+      <LinearGradient colors={this.props.gradientColors} style={_style.root}>
+        <View style={_style.container}>{children}</View>
+      </LinearGradient>;
     }
     return (
       <View style={_style.root}>
